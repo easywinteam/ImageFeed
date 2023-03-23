dd68f89f0f87f6efc272392104810708fe930a10
Maxim Malinovsky <gudzik@yahoo.com>
2023-03-23 11:50:52 +0200

    connection between table and storyboards

--- ImageFeed/ImagesListViewController.swift
+++ ImageFeed/ImagesListViewController.swift
@@ -11,3 +11,18 @@ class ImagesListViewController: UIViewController {
 
 }
 
+extension ImagesListViewController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
+
+extension ImagesListViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
+    }
+    
+}
