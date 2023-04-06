ee8e061eb77c8d85d5c01ca0a95ba2af80d3eb1a
Maxim Malinovsky <gudzik@yahoo.com>
2023-04-06 18:11:41 +0300

    ProfileViewController first version completed

--- ImageFeed/ProfileViewController.swift
+++ ImageFeed/ProfileViewController.swift
@@ -2,4 +2,14 @@ import UIKit
 
 final class ProfileViewController: UIViewController{
     
+    @IBOutlet var profileImage: UIImageView!
+    @IBOutlet var userFullName: UILabel!
+    @IBOutlet var userNickName: UILabel!
+    
+    @IBOutlet var userGreetings: UILabel!
+    
+    @IBAction func signOut(_ sender: Any) {
+        print("Max Cool")
+    }
+    
 }
