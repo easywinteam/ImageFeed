f3199c89cb1ea968bef8b50fd11109f856af2d38
Maxim Malinovsky <gudzik@yahoo.com>
2023-04-09 20:29:55 +0300

    Navigation Controller with segue image transferring was implemented

--- ImageFeed/SingleImageViewController.swift
+++ ImageFeed/SingleImageViewController.swift
@@ -1,7 +1,17 @@
 import UIKit
 
 final class SingleImageViewController: UIViewController{
-    
+    var image: UIImage!{
+        didSet{
+            guard isViewLoaded else {return}
+            imageView.image = image
+        }
+    }
     @IBOutlet var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = image
+    }
 }
 
