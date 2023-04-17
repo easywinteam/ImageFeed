a32c622b6e1cd9a012d414ae0b2a3408a21e0fc0
Maxim Malinovsky <gudzik@yahoo.com>
2023-04-17 14:48:13 +0300

    Profile screen was created

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -5,19 +5,22 @@ final class ProfileViewController: UIViewController{
     private var nameLabel: UILabel?
     private var profileImageView = UIImageView()
     private var exitProfileButton = UIButton()
+    private var nickLabel: UILabel?
+    private var userTextLabel: UILabel?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         createProfileImage()
         createNameLabel()
+        createNickLabel()
+        createUserTextLabel()
         createExitProfileButton()
         
     }
     
     private func createProfileImage(){
-        var profileImage = UIImage(systemName: "person.crop.circle.fill")
+        var profileImage = UIImage(named: "user_mock")
         profileImageView = UIImageView(image: profileImage)
-        profileImageView.tintColor = .gray
         profileImageView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(profileImageView)
         profileImageView.heightAnchor.constraint(equalToConstant: 70).isActive = true
@@ -31,13 +34,34 @@ final class ProfileViewController: UIViewController{
         nameLabel?.translatesAutoresizingMaskIntoConstraints = false
         nameLabel?.text = "Екатерина Новикова"
         nameLabel?.textColor = .white
+        nameLabel?.font = UIFont(name: "HelveticaNeue-Bold", size: 23)
         view.addSubview(nameLabel!)
         nameLabel?.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
         nameLabel?.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 8).isActive = true
     }
     
+    private func createNickLabel(){
+        nickLabel = UILabel()
+        nickLabel?.translatesAutoresizingMaskIntoConstraints = false
+        nickLabel?.text = "@ekaterinaNov"
+        nickLabel?.textColor = .gray
+        view.addSubview(nickLabel!)
+        nickLabel?.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        nickLabel?.topAnchor.constraint(equalTo: nameLabel!.bottomAnchor, constant: 8).isActive = true
+    }
+    
+    private func createUserTextLabel(){
+        userTextLabel = UILabel()
+        userTextLabel?.translatesAutoresizingMaskIntoConstraints = false
+        userTextLabel?.text = "Hello, world!"
+        userTextLabel?.textColor = .white
+        view.addSubview(userTextLabel!)
+        userTextLabel?.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        userTextLabel?.topAnchor.constraint(equalTo: nickLabel!.bottomAnchor, constant: 8).isActive = true
+    }
+    
     private func createExitProfileButton(){
-        exitProfileButton = UIButton.systemButton(with: UIImage(systemName: "ipad.and.arrow.forward")!, target: self, action: #selector(self.removeLabel))
+        exitProfileButton = UIButton.systemButton(with: UIImage(systemName: "ipad.and.arrow.forward")!, target: self, action: #selector(self.logOut))
         exitProfileButton.translatesAutoresizingMaskIntoConstraints = false
         exitProfileButton.tintColor = .red
         view.addSubview(exitProfileButton)
@@ -45,9 +69,8 @@ final class ProfileViewController: UIViewController{
         exitProfileButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
     }
     
-    @objc func removeLabel(){
-        nameLabel?.removeFromSuperview()
-        print("Selector was called")
+    @objc func logOut(){
+
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
