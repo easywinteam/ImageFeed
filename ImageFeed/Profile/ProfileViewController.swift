f148cf2b44382920f37b603cfacddf17bbf5293e
Maxim Malinovsky <gudzik@yahoo.com>
2023-04-17 13:18:41 +0300

    Code Layout for ProfileViewController% avatarImage + button + label

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -2,14 +2,54 @@ import UIKit
 
 final class ProfileViewController: UIViewController{
     
-    @IBOutlet var avatarImageView: UIImageView!
-    @IBOutlet var nameLabel: UILabel!
-    @IBOutlet var LoginNameLabel: UILabel!
+    private var nameLabel: UILabel?
+    private var profileImageView = UIImageView()
+    private var exitProfileButton = UIButton()
     
-    @IBOutlet var descriptionLabel: UILabel!
-    
-    @IBAction func logOut(_ sender: Any) {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        createProfileImage()
+        createNameLabel()
+        createExitProfileButton()
+        
     }
+    
+    private func createProfileImage(){
+        var profileImage = UIImage(systemName: "person.crop.circle.fill")
+        profileImageView = UIImageView(image: profileImage)
+        profileImageView.tintColor = .gray
+        profileImageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(profileImageView)
+        profileImageView.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        profileImageView.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        profileImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        profileImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+    }
+    
+    private func createNameLabel(){
+        nameLabel = UILabel()
+        nameLabel?.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel?.text = "Екатерина Новикова"
+        nameLabel?.textColor = .white
+        view.addSubview(nameLabel!)
+        nameLabel?.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        nameLabel?.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 8).isActive = true
+    }
+    
+    private func createExitProfileButton(){
+        exitProfileButton = UIButton.systemButton(with: UIImage(systemName: "ipad.and.arrow.forward")!, target: self, action: #selector(self.removeLabel))
+        exitProfileButton.translatesAutoresizingMaskIntoConstraints = false
+        exitProfileButton.tintColor = .red
+        view.addSubview(exitProfileButton)
+        exitProfileButton.centerYAnchor.constraint(equalTo: profileImageView.centerYAnchor).isActive = true
+        exitProfileButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+    }
+    
+    @objc func removeLabel(){
+        nameLabel?.removeFromSuperview()
+        print("Selector was called")
+    }
+    
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
