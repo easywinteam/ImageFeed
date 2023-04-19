044c548647886b8dd5b9577907bd1d24153df162
Maxim Malinovsky <gudzik@yahoo.com>
2023-04-19 17:31:00 +0300

    Fixed mistake with URL

--- ImageFeed/Constants.swift
+++ ImageFeed/Constants.swift
@@ -4,4 +4,4 @@ let AccessKey = "BBJY-Gy6oI9z0y90NFXGlQGpvpnwyDztu9TerNlqnRU"
 let SecretKey = "-ICi0jLP2NsUJhNpC5cEshxotyFkYo2Hl0PC-EKLlQs"
 let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
 let AccessScope = "public+read_user+write_likes"
-let DefaultBaseURL = URL(string: "https://api.unsplash.com")
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
