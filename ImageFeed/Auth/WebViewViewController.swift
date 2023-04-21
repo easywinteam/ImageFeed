2bf467adf9b607e625391e87460ca6baff6f27bf
Maxim Malinovsky <gudzik@yahoo.com>
2023-04-21 09:26:17 +0300

    Web View Unsplash was shown

--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -5,6 +5,22 @@ final class WebViewViewController: UIViewController{
     
     @IBOutlet var webView: WKWebView!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var urlComponents = URLComponents(string: UnsplashAuthorizeURLString)! 
+        urlComponents.queryItems = [
+           URLQueryItem(name: "client_id", value: AccessKey),                  //2
+           URLQueryItem(name: "redirect_uri", value: RedirectURI),             //3
+           URLQueryItem(name: "response_type", value: "code"),                 //4
+           URLQueryItem(name: "scope", value: AccessScope)                     //5
+         ]
+        let url = urlComponents.url!
+        let request = URLRequest(url: url)
+        webView.load(request)
+        
+    }
+    
     
     @IBAction func didTapBackButton(_ sender: Any) {
     }
