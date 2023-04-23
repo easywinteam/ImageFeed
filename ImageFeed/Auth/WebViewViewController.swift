526c87f4836562abb6b5e8e3c6637b3a94522815
Maxim Malinovsky <gudzik@yahoo.com>
2023-04-23 13:35:37 +0300

    outlet for progress view

--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -4,6 +4,9 @@ import WebKit
 final class WebViewViewController: UIViewController{
     
     @IBOutlet var webView: WKWebView!
+    
+    @IBOutlet private var progressView: UIProgressView!
+    
     weak var delegate: WebViewViewControllerDelegate?
     
     override func viewDidLoad() {
@@ -11,13 +14,13 @@ final class WebViewViewController: UIViewController{
         
         webView.navigationDelegate = self
         
-        var urlComponents = URLComponents(string: UnsplashAuthorizeURLString)! 
+        var urlComponents = URLComponents(string: UnsplashAuthorizeURLString)!
         urlComponents.queryItems = [
-           URLQueryItem(name: "client_id", value: AccessKey),                  //2
-           URLQueryItem(name: "redirect_uri", value: RedirectURI),             //3
-           URLQueryItem(name: "response_type", value: "code"),                 //4
-           URLQueryItem(name: "scope", value: AccessScope)                     //5
-         ]
+            URLQueryItem(name: "client_id", value: AccessKey),                  //2
+            URLQueryItem(name: "redirect_uri", value: RedirectURI),             //3
+            URLQueryItem(name: "response_type", value: "code"),                 //4
+            URLQueryItem(name: "scope", value: AccessScope)                     //5
+        ]
         let url = urlComponents.url!
         let request = URLRequest(url: url)
         webView.load(request)
--- ImageFeed/Auth/WebViewViewControllerDelegate.swift
+++ ImageFeed/Auth/WebViewViewControllerDelegate.swift
@@ -2,6 +2,8 @@ import UIKit
 
 protocol WebViewViewControllerDelegate: AnyObject{
     
+    
+    
     func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String)
     
     func webViewViewControllerDidCancel(_ vc: WebViewViewController)
