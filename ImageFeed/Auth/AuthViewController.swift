90cae64638279b02a244668a0b4b07a532283500
Maxim Malinovsky <gudzik@yahoo.com>
2023-05-18 13:33:06 +0300

    StyleGuide changes

--- ImageFeed/Auth/AuthViewController.swift
+++ ImageFeed/Auth/AuthViewController.swift
@@ -1,16 +1,16 @@
 import UIKit
 
-protocol AuthViewControllerDelegate: AnyObject{
+protocol AuthViewControllerDelegate: AnyObject {
     func authViewController(_ vc: AuthViewController, didAuthenticateWithCode code: String)
 }
 
-final class AuthViewController: UIViewController{
+final class AuthViewController: UIViewController {
     let WebViewSegueIdentifier = "ShowWebView"
     
     weak var delegate: AuthViewControllerDelegate?
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == WebViewSegueIdentifier{
+        if segue.identifier == WebViewSegueIdentifier {
             guard let webViewViewController = segue.destination as? WebViewViewController else{ fatalError("Failed to prepare for \(WebViewSegueIdentifier)")
             }
             webViewViewController.delegate = self
@@ -20,7 +20,7 @@ final class AuthViewController: UIViewController{
     }
 }
 
-extension AuthViewController: WebViewViewControllerDelegate{
+extension AuthViewController: WebViewViewControllerDelegate {
     func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
         //TODO: process code
         delegate?.authViewController(self, didAuthenticateWithCode: code)
