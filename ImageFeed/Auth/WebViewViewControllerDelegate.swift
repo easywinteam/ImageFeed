d6c7a4f68449309ce20577f2a44e907c7284501e
Maxim Malinovsky <gudzik@yahoo.com>
2023-05-18 21:16:30 +0300

    Improved spaces between strings

--- ImageFeed/Auth/WebViewViewControllerDelegate.swift
+++ ImageFeed/Auth/WebViewViewControllerDelegate.swift
@@ -2,10 +2,7 @@ import UIKit
 
 protocol WebViewViewControllerDelegate: AnyObject{
     
-    
-    
     func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String)
     
     func webViewViewControllerDidCancel(_ vc: WebViewViewController)
-    
 }
