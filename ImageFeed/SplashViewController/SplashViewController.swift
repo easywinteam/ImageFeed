d3ee0d9ae362e7091fddc1d6bb7d3c3b485bf730
Maxim Malinovsky <gudzik@yahoo.com>
2023-05-23 13:23:52 +0300

    ProgressHUD on splash screen was implemented

--- ImageFeed/SplashViewController/SplashViewController.swift
+++ ImageFeed/SplashViewController/SplashViewController.swift
@@ -52,6 +52,7 @@ extension SplashViewController {
 
 extension SplashViewController: AuthViewControllerDelegate {
     func authViewController(_ vc: AuthViewController, didAuthenticateWithCode code: String) {
+        ProgressHUD.show()
         dismiss(animated: true) { [weak self] in
             guard let self = self else { return }
             self.fetchOAuthToken(code)
@@ -64,7 +65,9 @@ extension SplashViewController: AuthViewControllerDelegate {
             switch result {
             case .success:
                 self.switchToTabBarController()
+                ProgressHUD.dismiss()
             case .failure:
+                ProgressHUD.dismiss()
                 // TODO [Sprint 11]
                 break
             }
