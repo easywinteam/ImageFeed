a2361bab83e8082a7355743c3059906e59981dec
Maxim Malinovsky <gudzik@yahoo.com>
2023-05-25 15:41:02 +0300

    Fixing race condition by locking UI

--- ImageFeed/SplashViewController/SplashViewController.swift
+++ ImageFeed/SplashViewController/SplashViewController.swift
@@ -52,7 +52,7 @@ extension SplashViewController {
 
 extension SplashViewController: AuthViewControllerDelegate {
     func authViewController(_ vc: AuthViewController, didAuthenticateWithCode code: String) {
-        ProgressHUD.show()
+        UIBlockingProgressHUD.show()
         dismiss(animated: true) { [weak self] in
             guard let self = self else { return }
             self.fetchOAuthToken(code)
@@ -65,10 +65,9 @@ extension SplashViewController: AuthViewControllerDelegate {
             switch result {
             case .success:
                 self.switchToTabBarController()
-                ProgressHUD.dismiss()
+                UIBlockingProgressHUD.dismiss()
             case .failure:
-                ProgressHUD.dismiss()
-                // TODO [Sprint 11]
+                UIBlockingProgressHUD.dismiss()
                 break
             }
         }
