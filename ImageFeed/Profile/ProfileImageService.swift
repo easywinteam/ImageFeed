782ecb7091b40a6ae7800102654c0af981ec7564
Maxim Malinovsky <gudzik@yahoo.com>
2023-06-09 19:49:04 +0300

    fetchProfileImageUrl

--- ImageFeed/Profile/ProfileImageService.swift
+++ ImageFeed/Profile/ProfileImageService.swift
@@ -1,7 +1,44 @@
 import UIKit
 
 final class ProfileImageService {
+    private let tokenStorage = OAuth2TokenStorage()
+    private let urlSession = URLSession.shared
+    private (set) var avatarURL: String?
     
+    private var task: URLSessionTask?
+    
+    static let didChangeNotification = Notification.Name("ProfileImageProviderDidChange")
+    
+    func fetchProfileImageURL(userName: String, _ completion: @escaping (Result<String, Error>) -> Void) {
+        guard let token = tokenStorage.token else {return}
+        
+        let urlString = "https://api.unsplash.com/users/\(userName)"
+        guard let url = URL(string: urlString) else { return }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        
+        let dataTask = urlSession.objectTask(for: request) { [weak self] (result: Result<UserResult, Error>) in
+            switch result {
+            case .success(let userResult):
+                self?.avatarURL = userResult.profileImage.small
+                if let avatarURL = self?.avatarURL {
+                    completion(.success(userResult.profileImage.small))
+                    NotificationCenter.default.post(name: ProfileImageService.didChangeNotification,
+                                                    object: self,
+                                                    userInfo: ["URL": userResult.profileImage.small])
+                } else {
+                    completion(.failure(ProfileServiceError.invalidData))
+                }
+                self?.task = nil
+            case .failure(_):
+                completion(.failure(ProfileServiceError.decodingFailed))
+            }
+        }
+        task = dataTask
+        task?.resume()
+    }
 }
 
 struct UserResult: Codable {
