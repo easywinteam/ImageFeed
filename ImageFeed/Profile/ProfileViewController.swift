e64c2e4c0a37f28bcb6668c86e2dd8b9e5836565
Maxim Malinovsky <gudzik@yahoo.com>
2023-07-12 19:35:17 +0300

    small code cleaning

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -115,7 +115,7 @@ final class ProfileViewController: UIViewController{
             let profileImageURL = ProfileImageService.shared.avatarURL,
             let url = URL(string: profileImageURL)
         else { return }
-        // TODO [Sprint 11] обновить аватар, используя кингфишер
+        
         let placeholderImage = UIImage(named: "user_mock")
         profileImageView.kf.setImage(with: url, placeholder: placeholderImage)
     }
